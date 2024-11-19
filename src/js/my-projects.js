b731995d506a837a7ffac27b41f03a35dd47a582
DGOLD7788 <80936904545dgold@gmail.com>
2024-11-19 15:39:54 +0200

    add

---
+++ src/js/my-projects.js
@@ -0,0 +1,129 @@
+import myprojects1_1x from "../img/my-projects_img/desk-myprojects1_1x.jpg";
+import myprojects2_1x from "../img/my-projects_img/desk-myprojects2_1x.jpg";
+import myprojects3_1x from "../img/my-projects_img/desk-myprojects3_1x.jpg";
+import myprojects4_1x from "../img/my-projects_img/desk-myprojects4_1x.jpg";
+import myprojects5_1x from "../img/my-projects_img/desk-myprojects5_1x.jpg";
+import myprojects6_1x from "../img/my-projects_img/desk-myprojects6_1x.jpg";
+import myprojects7_1x from "../img/my-projects_img/desk-myprojects7_1x.jpg";
+import myprojects8_1x from "../img/my-projects_img/desk-myprojects8_1x.jpg";
+import myprojects9_1x from "../img/my-projects_img/desk-myprojects9_1x.jpg";
+import myprojects10_1x from "../img/my-projects_img/desk-myprojects10_1x.jpg";
+import myprojects1_2x from "../img/my-projects_img/desk-myprojects1_2x.jpg";
+import myprojects3_2x from "../img/my-projects_img/desk-myprojects3_2x.jpg";
+import myprojects2_2x from "../img/my-projects_img/desk-myprojects2_2x.jpg";
+import myprojects5_2x from "../img/my-projects_img/desk-myprojects5_2x.jpg";
+import myprojects4_2x from "../img/my-projects_img/desk-myprojects4_2x.jpg";
+import myprojects6_2x from "../img/my-projects_img/desk-myprojects6_2x.jpg";
+import myprojects7_2x from "../img/my-projects_img/desk-myprojects7_2x.jpg";
+import myprojects8_2x from "../img/my-projects_img/desk-myprojects8_2x.jpg";
+import myprojects9_2x from "../img/my-projects_img/desk-myprojects9_2x.jpg";
+import myprojects10_2x from "../img/my-projects_img/desk-myprojects10_2x.jpg";
+
+const projects = [
+  {
+    imageBase_1x: myprojects1_1x,
+    imageBase_2x: myprojects1_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Power pulse webservice ",
+  },
+  {
+    imageBase_1x: myprojects2_1x,
+    imageBase_2x: myprojects2_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Mimino website",
+  },
+  {
+    imageBase_1x: myprojects3_1x,
+    imageBase_2x: myprojects3_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Vyshyvanka vibes Landing Page",
+  },
+  {
+    imageBase_1x: myprojects4_1x,
+    imageBase_2x: myprojects4_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Green harvest online store",
+  },
+  {
+    imageBase_1x: myprojects5_1x,
+    imageBase_2x: myprojects5_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Wallet webservice",
+  },
+  {
+    imageBase_1x: myprojects6_1x,
+    imageBase_2x: myprojects6_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Chego jewelry website",
+  },
+  {
+    imageBase_1x: myprojects7_1x,
+    imageBase_2x: myprojects7_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Energy flow webservice ",
+  },
+  {
+    imageBase_1x: myprojects8_1x,
+    imageBase_2x: myprojects8_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Fruitbox online store",
+  },
+  {
+    imageBase_1x: myprojects9_1x,
+    imageBase_2x: myprojects9_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "English excellence webservice",
+  },
+  {
+    imageBase_1x: myprojects10_1x,
+    imageBase_2x: myprojects10_2x,
+    technologies: "React, JavaScript, Node JS, Git",
+    title: "Starlight studio landing page",
+  },
+];
+
+const galleryEl = document.querySelector(".my-projects-list");
+const moreBtnEl = document.querySelector(".more-btn");
+
+let currentIndex = 0;
+const projectsPerPage = 3;
+
+const createProjectMarkup = (project) => {
+  return `
+<li class="projects-item">
+      <picture>
+        <source srcset="${project.imageBase_1x} 1x, ${project.imageBase_2x} 2x" media="(min-width: 1280px)">
+        <source srcset="${project.imageBase_1x} 1x, ${project.imageBase_2x} 2x" media="(min-width: 768px)">
+        <source srcset="${project.imageBase_1x} 1x, ${project.imageBase_2x} 2x" media="(max-width: 767px)">
+        <img class="project-img" src="${project.imageBase_1x}" alt="${project.title}">
+      </picture>
+      <p class="technologies">${project.technologies}</p>
+      <div class="title-btn-wrapper">
+        <p class="my-project-title">${project.title}</p>
+        <button class="visit-btn" type="button" onclick="window.open('https://github.com/dgold4545/masterFront', '_blank');">
+          Visit
+          <svg class="visit-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+            <path fill="none" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" stroke=#00b068 d="M6.333 25.667l19.333-19.333M25.667 6.333h-18M25.667 6.333v18"></path>
+          </svg>
+        </button>
+      </div>
+    </li>`;
+};
+
+const loadProjects = () => {
+  const nextIndex = currentIndex + projectsPerPage;
+  const projectsToLoad = projects.slice(currentIndex, nextIndex);
+
+  const markup = projectsToLoad.map(createProjectMarkup).join("");
+  galleryEl.insertAdjacentHTML("beforeend", markup);
+
+  currentIndex = nextIndex;
+
+  if (currentIndex >= projects.length) {
+    moreBtnEl.classList.add("btn-is-hidden");
+  }
+};
+
+moreBtnEl.addEventListener("click", loadProjects);
+
+loadProjects();
---
+++ src/js/work-together.js
@@ -0,0 +1,77 @@
+import { openPopupModal } from "./popup-modal";
+//openPopupModal();
+const contactsForm = document.querySelector('.contacts-form');
+const inputEmail = document.querySelector('.contacts-input');
+const inputMessage = document.getElementById('contacts-message');
+const inputErrorEmail = document.querySelector('.contacts-error-email');
+const inputErrorMessage = document.querySelector('.contacts-error-message');
+const checkboxIconEmail = document.querySelector('.contacts-checkbox-icon-email');
+const checkboxIconMessage = document.querySelector('.contacts-checkbox-icon-message');
+
+contactsForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!inputEmail.value.match(/^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+        inputEmail.style.color = '#E74A3B';
+        inputErrorEmail.style.display = 'inline';
+        checkboxIconEmail.style.display = 'none';
+    }
+    else if (inputMessage.value.trim() == "") {
+        inputErrorMessage.style.display = 'inline';
+        checkboxIconMessage.style.display = 'none';
+    }
+    else {
+        const formData = {
+            email: inputEmail.value,
+            message: inputMessage.value
+        };
+        fetch('https://formspree.io/f/mvoejbkp', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => {
+            if (response.ok) {
+                openPopupModal();
+                contactsForm.reset();
+                checkboxIconEmail.style.display = 'none';
+                checkboxIconMessage.style.display = 'none';
+            } else {
+                console.error('Error submitting form:', response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('Network error:', error);
+        });
+    }
+})
+inputEmail.addEventListener('input', () => {
+    inputEmail.style.color = '#292929';
+    if (document.body.classList.contains('light-mode')) {
+         inputEmail.style.color = '#292929';
+     }
+    if (document.body.classList.contains('dark-mode')) {
+         inputEmail.style.color = '#F0F0F0';
+     }
+    inputErrorEmail.style.display = 'none';
+    if (inputEmail.value.match(/^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+        checkboxIconEmail.style.display = 'block';
+    }
+    else {
+        checkboxIconEmail.style.display = 'none';
+    }
+})
+inputMessage.addEventListener('input', () => {
+    inputErrorMessage.style.display = 'none';
+    inputMessage.value = inputMessage.value.replace(/^\s+/, '');
+    inputMessage.value = inputMessage.value.replace(/\s{2,}/g, ' ');
+    if (inputMessage.value != "") {
+        checkboxIconMessage.style.display = 'block';
+    }
+    else {
+        checkboxIconMessage.style.display = 'none';
+        
+    }
+})
+//https://formspree.io/forms/mvoejbkp/submissions
